d639c4f5e0e691a281739afe00e6d82fd82b804f
Jing Fu <jingfu@JingdeMacBook-Air.local>
2021-01-07 17:17:56 +0800

    注释

--- get-quote.py
+++ get-quote.py
@@ -2,11 +2,11 @@ import random
 def primary():
   #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+  f = open("quotes.txt")  #打开quotes.txt文件
+  quotes = f.readlines()  #将文件里每一行内容存入数组
+  f.close()               #关闭文件
   last = 13
-  rnd = random.randint(0,last)
+  rnd = random.randint(0,last) #随机抽取一个数字（0，13）
   print(quotes[rnd])
 
 if __name__== "__main__":
